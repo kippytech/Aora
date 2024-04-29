75996f2dcfc0e5d30e0b490aa90ae0695836db68
kippytech <dkipyegon.dk7.dk@gmail.com>
2024-04-29 15:54:17 +0300

    file & folder structure

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className='flex-1 items-center justify-center bg-white'>
+      <Text className='text-3xl font-pblack'>Aora!</Text>
+      <StatusBar style="auto" />
+      <Link href='/home' style={{ color: 'blue' }}>Go to Home</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
